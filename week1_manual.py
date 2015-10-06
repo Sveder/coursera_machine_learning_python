82437693b8df3afd2e694e670f585f8a050c8f4f
Sveder <m@sveder.com>
2015-10-06 18:23:43 +0300

    Some code works :)

--- week1_manual.py
+++ week1_manual.py
@@ -8,6 +8,14 @@ DATA_SET = {0: 2,
 HYPO_FUNCTION = (2, 2)
 
 
+def generate_perfect_data(theta_0, theta_1, x_range):
+    d = {}
+    for i in x_range:
+        d[i] = theta_0 + i * theta_1
+
+    return d
+
+
 def hypo_function(theta_0, theta_1):
     def the_func(x):
         return theta_0 + theta_1 * x
@@ -15,10 +23,10 @@ def hypo_function(theta_0, theta_1):
     return the_func
 
 
-def cost_function(data_set, hypo_fucntion):
+def cost_function(data_set, hypo_function):
     cost = 0.0
     for i, (x, y) in enumerate(data_set.items()):
-        cost += (hypo_fucntion(x) - y) ** 2
+        cost += (hypo_function(x) - y) ** 2
 
     return cost / (2 * len(data_set))
 
@@ -52,4 +60,6 @@ def linear_gradient_descent(data_set, theta_0, theta_1, step):
     return new_theta_0, new_theta_1
 
 
-print linear_gradient_descent(DATA_SET, 3, 3, 0.1)
+print cost_function(generate_perfect_data(2, 1, range(20)), hypo_function(-2, -2))
+
+# print linear_gradient_descent(generate_perfect_data(2, 1, range(5)), 3, 3, 0.1)
--- week1_sci.py
+++ week1_sci.py
@@ -1,12 +1,58 @@
-#First I want to see a plot of the data set
-#Then I want to see the bowl thingy of the cost function
-#then I want to see regression step by step?
+"""
+Done: plots points.
 
-DATA_SET = {0: 2,
-            1: 4,
-            2: 6,
-            3: 8}
+TODO:
+1. Make the cost function plot be a bowl instead of the half bowl it is now. - HINT - if that doesn't work, revert to
+one variable like the example, and debug it until I get a U shape.
+2. Make a visual linear regression "video" or whatever frame by frame matplot can do.
+"""
+
+from mpl_toolkits.mplot3d import Axes3D
+from matplotlib import cm
+from matplotlib.ticker import LinearLocator, FormatStrFormatter
+import matplotlib.pyplot as plt
+import numpy as np
+
+import week1_manual
 
 
 def plot(data_set):
-    
+    plt.scatter(data_set.keys(), data_set.values())
+    plt.show()
+
+
+def plot_cost_function(data_set):
+    fig = plt.figure()
+    ax = fig.gca(projection='3d')
+
+    X = np.arange(-2, 6, 0.25)
+    Y = np.arange(-2, 6, 0.25)
+
+    X, Y = np.meshgrid(X, Y)
+
+    Z = np.array([week1_manual.cost_function(data_set, week1_manual.hypo_function(x, y)) for x, y in zip(np.ravel(X), np.ravel(Y))])
+
+    print X
+
+    print "-------"
+
+    print Z
+    # for i in X:
+    #     print "i=", i
+    #     for j in Y:
+    #         print "j=", i
+    #         for k in Z:
+    #             print "k=", k
+    #             raw_input()
+
+    surf = ax.plot_surface(X, Y, Z, rstride=1, cstride=1, cmap=cm.coolwarm,
+             linewidth=0, antialiased=False)
+
+    fig.colorbar(surf, shrink=0.5, aspect=5)
+
+    plt.show()
+
+
+plot_cost_function(week1_manual.generate_perfect_data(2, 1, range(20)))
+
+#
